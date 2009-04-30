add074041ae4f32a548116e5cb3bf115753a711f
David Golden <dagolden@cpan.org>
2009-04-30 10:51:17 -0400

    bump version to 0.19

--- lib/Getopt/Lucid.pm
+++ lib/Getopt/Lucid.pm
@@ -3,7 +3,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 $VERSION = eval $VERSION;
 our @EXPORT_OK = qw(Switch Counter Param List Keypair);
 our %EXPORT_TAGS = ( all => [ @EXPORT_OK ] );
