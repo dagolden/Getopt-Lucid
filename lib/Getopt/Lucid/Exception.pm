e39a85a03d838020d53eb0490b4a567b550d398b
David Golden <xdg@xdg.me>
2019-10-25 17:15:05 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Getopt/Lucid/Exception.pm
+++ lib/Getopt/Lucid/Exception.pm
@@ -4,7 +4,7 @@ use warnings;
 package Getopt::Lucid::Exception;
 # ABSTRACT: Exception classes for Getopt::Lucid
 
-our $VERSION = '1.10';
+our $VERSION = '1.11';
 
 use Exporter;
 our @ISA = qw/Exporter Exception::Class::Base/;
